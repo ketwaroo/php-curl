4b9fcd882ef5559d6352633ed91a2ccf60f000fd
Yaasir Ketwaroo <ketwaroo.yaasir@gmail.com>
2022-09-06 16:49:46 -0400

    Modified   src/Ketwaroo/Curl/Response.php

--- src/Ketwaroo/Curl/Response.php
+++ src/Ketwaroo/Curl/Response.php
@@ -7,48 +7,44 @@ namespace Ketwaroo\Curl;
  *
  * @author Yaasir Ketwaroo
  */
-class Response
-{
+class Response {
 
     protected $info, $header, $body;
 
-    public function __construct($usedConnection, $returnedTransfer)
-    {
+    public function __construct($usedConnection, $returnedTransfer) {
         $this->info = curl_getinfo($usedConnection);
 
         $this->header = substr($returnedTransfer, 0, $this->info['header_size']);
         $this->body   = substr($returnedTransfer, $this->info['header_size']);
     }
 
-    public function getInfo()
-    {
+    public function getInfo() {
         return $this->info;
     }
 
-    public function getHeader()
-    {
+    public function getRequestUrl() {
+        return $this->getInfo()['url'] ?? null;
+    }
+
+    public function getHeader() {
         return $this->header;
     }
 
-    public function getBody()
-    {
+    public function getBody() {
         return $this->body;
     }
 
-    public function setInfo($info)
-    {
+    public function setInfo($info) {
         $this->info = $info;
         return $this;
     }
 
-    public function setHeader($header)
-    {
+    public function setHeader($header) {
         $this->header = $header;
         return $this;
     }
 
-    public function setBody($body)
-    {
+    public function setBody($body) {
         $this->body = $body;
         return $this;
     }
